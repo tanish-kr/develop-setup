3a5caa8c270f48ef3e17323e17d281f0a2382e8f
tanish-kr <tora.1986.tatsu@gmail.com>
2021-12-23 10:45:11 +0900

    added chocolatey install script

---
+++ chocolatey_install.ps1
@@ -0,0 +1,18 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install -y googlechrome
+choco install -y slack
+choco install -y docker-desktop
+choco install -y vscode
+choco install -y googlejapaneseinput
+choco install -y vim
+choco install -y office365business
+choco install -y postman
+choco install -y virtualbox
+choco install -y steam-client
+choco install -y lhaplus
+choco install -y microsoft-windows-terminal
+choco install -y cmder
+choco install -y kindle
+choco install -y avastfreeantivirus
+
